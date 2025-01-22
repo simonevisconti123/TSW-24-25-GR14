14db8375f3f9dab10db0872e5aa13d080b592f4e
simonevisconti123 <simovis003@gmail.com>
2025-01-22 21:42:46 +0100

    pagina presentazione simone + php contatti

---
+++ php/php_contattami.php
@@ -0,0 +1,23 @@
+<?php
+    
+    
+    // Aggiungere validazione degli input
+    // Dire agli altri membri di modificare il campo hidden come l'if per forza
+    
+    
+    $destinazione = "";
+    $oggetto = "Nuovo messaggio da " . $_POST["nome"];
+    $messaggio = $_POST["messaggio"];
+    if($_POST["source_page"] == "simonevisconti"){
+        $destinazione = "s.visconti3@studenti.unisa.it";
+    }else if($_POST["source_page"] == "vincenzogoffredo"){
+        $destinazione = "v.goffredo@studenti.unisa.it";
+    }else if($_POST["source_page"] == "anthonyvita"){
+        $destinazione = "a.vita9@studenti.unisa.it";
+    }else if($_POST["source_page"] == "liviaciasullo"){
+        $destinazione = "l.ciasullo1@studenti.unisa.it";
+    }
+
+    mail($destinazione,$oggetto,$messaggio);
+
+?>
