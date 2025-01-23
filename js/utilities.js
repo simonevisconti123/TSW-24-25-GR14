77c86af3aa3c98a5284d76cc9d4bb5c6c7bf7d1c
simonevisconti123 <simovis003@gmail.com>
2025-01-23 12:21:01 +0100

    spostamento del check in utilities

--- js/utilities.js
+++ js/utilities.js
@@ -5,3 +5,22 @@ function returnToHome() {
 function returnToLogin() {
     window.location = "login.html";
 }
+
+function check_input(modulo){
+    var regex_username = "[a-zA-Z0-9]";
+    var regex_email = "[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}";
+    if(modulo.nome.value == "" || !(modulo.nome.value.match(regex_username))){
+        if(modulo.email.value == "" || !(modulo.email.value.match(regex_email))){
+            document.getElementById("label_functional").innerHTML = "Mail e username sono nel formato scorretto!";
+            return false;
+        }else{
+            document.getElementById("label_functional").innerHTML = "Lo username è nel formato scorretto!";
+            return false;
+        }
+    }else if(modulo.email.value == "" || !(modulo.email.value.match(regex_email))){
+        document.getElementById("label_functional").innerHTML = "La mail è nel formato scorretto!";
+        return false;
+    }else{
+        return true;
+    }
+}
