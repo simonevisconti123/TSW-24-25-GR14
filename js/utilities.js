880265fb3d2bc9408fc3e34da5f0ca5048a14370
simonevisconti123 <simovis003@gmail.com>
2025-02-04 19:11:54 +0100

    Utenti registrabili

--- js/utilities.js
+++ js/utilities.js
@@ -26,4 +26,31 @@ function check_input(modulo) {
     return true;
 }
 
-  
+
+function check_input_registration(modulo) {
+    var regex_username = /^[a-zA-Z0-9]+$/;
+    var regex_email = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    var regex_pswd = /^(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$/;
+    if (!regex_username.test(modulo.inputUserName.value)) {
+        document.getElementById("label_output").innerHTML = "Il nome può contenere solo lettere e numeri";
+        return false;
+    }
+
+    if (!regex_email.test(modulo.inputMail.value)) {
+        document.getElementById("label_output").innerHTML = "L'email non è nel formato corretto.";
+        return false;
+    }
+
+    if  (!regex_pswd.test(modulo.inputPassword.value)) {
+        document.getElementById("label_output").innerHTML = ` La password non rispetta i requisiti:
+    <ul>
+        <li>Almeno 8 caratteri</li>
+        <li>Almeno 1 maiuscolo</li>
+        <li>1 numero e 1 carattere speciale</li>
+    </ul>
+`;
+        return false;
+    }
+
+    return true;
+}
--- php/registration.php
+++ php/registration.php
@@ -8,17 +8,29 @@ $connection_string = "host=$host dbname=$db user=$username password=$password";
 
 $db = pg_connect($connection_string)
 or die('Impossibile connetersi al database: ' . pg_last_error());
-echo "Connessione al database riuscita<br/>";
 
 $user = $_POST["inputUserName"];
 $email = $_POST["inputMail"];
 $pswd = $_POST["inputPassword"];
 
+$secret_salt = "you_will_never_guess_me";
+$hashed_pswd = hash('sha256', $secret_salt.$pswd);
+
 $result = pg_prepare($db,"test_prepared","INSERT INTO utenti VALUES ($1,'url_pro_pic_here',$2,$3,'23,24,25');");
-$ret = pg_execute($db, "test_prepared", array($user,$email,$pswd));
+$ret = pg_execute($db, "test_prepared", array($user,$email,$hashed_pswd));
 
 if($ret == false){
-    echo("errore");
+    $response = array(
+        "message" => "Errore nella query"
+    );
+    echo json_encode($response);
+    exit;
+}else{
+    $response = array(
+        "message" => "Inserimento corretto"
+    );
+    echo json_encode($response);
+    exit;
 }
 
 ?>
