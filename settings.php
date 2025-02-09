0e1d6bbe21c805fbacadb9ec5883afa2ad9b4e50
simonevisconti123 <simovis003@gmail.com>
2025-02-09 18:34:15 +0100

    fix favicon

--- settings.php
+++ settings.php
@@ -10,6 +10,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Il Mio Account</title>
+    <link rel="shortcut icon" type="image/x-icon" href="img/favicon.ico">
     <link rel="stylesheet" href="css/settings_style.css">
     <script src="js/utilities.js"></script>
 
