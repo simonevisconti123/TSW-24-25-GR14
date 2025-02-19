76c3b3c96ef7e7f2e74e2be2bde3ccc3d8f7e4c6
VinnGoff <v.goffredo@studenti.unisa.it>
2025-02-19 17:47:04 +0100

    piccoli fix grafici registration e login

--- registration.php
+++ registration.php
@@ -52,6 +52,9 @@ if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST["inputUserName"], $_PO
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registrati</title>
     <link rel="stylesheet" href="css/registration_style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Lexend+Mega:wght@553&display=swap" rel="stylesheet">
     <script src="js/utilities.js"></script>
 </head>
 <body>
